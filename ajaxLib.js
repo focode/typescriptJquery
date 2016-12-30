e4bc32f5d834eedd9ecd06266a351862cc36c23b
Sunil Soni <sunilsoni@live.in>
2016-12-30 17:37:48 +0530

    using amd

--- ajaxLib.js
+++ ajaxLib.js
@@ -1,10 +1,12 @@
-"use strict";
-var AjaxContainer = (function () {
-    function AjaxContainer(value) {
-        var el = document.getElementById('grid2');
-        $(el).css({ "color": "blue" });
-    }
-    return AjaxContainer;
-}());
-exports.AjaxContainer = AjaxContainer;
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    var AjaxContainer = (function () {
+        function AjaxContainer(value) {
+            var el = document.getElementById('grid2');
+            $(el).css({ "color": "blue" });
+        }
+        return AjaxContainer;
+    }());
+    exports.AjaxContainer = AjaxContainer;
+});
 //# sourceMappingURL=ajaxLib.js.map
--- home.js
+++ home.js
@@ -1,18 +1,19 @@
-"use strict";
-var ajaxLib_1 = require("./ajaxLib");
-var Home;
-(function (Home) {
-    var HomeContainer = (function () {
-        function HomeContainer(value) {
-            var animal = new ajaxLib_1.AjaxContainer();
-            var el = document.getElementById('grid1');
-            $(el).css({ "color": "blue" });
-        }
-        return HomeContainer;
-    }());
-    Home.HomeContainer = HomeContainer;
-})(Home || (Home = {}));
-window.onload = function () {
-    var object1 = new Home.HomeContainer("ram");
-};
+define(["require", "exports", "./ajaxLib"], function (require, exports, ajaxLib_1) {
+    "use strict";
+    var Home;
+    (function (Home) {
+        var HomeContainer = (function () {
+            function HomeContainer(value) {
+                var animal = new ajaxLib_1.AjaxContainer();
+                var el = document.getElementById('grid1');
+                $(el).css({ "color": "blue" });
+            }
+            return HomeContainer;
+        }());
+        Home.HomeContainer = HomeContainer;
+    })(Home || (Home = {}));
+    window.onload = function () {
+        var object1 = new Home.HomeContainer("ram");
+    };
+});
 //# sourceMappingURL=home.js.map
